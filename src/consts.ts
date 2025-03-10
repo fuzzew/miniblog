d0eba259bfa053200604dbf4d2412142c48b1e20
Fuzzy <fuzzew@gmail.com>
2025-03-10 14:43:19 +0530

    Fix email

--- src/consts.ts
+++ src/consts.ts
@@ -2,4 +2,4 @@ export const SITE_URL = "https://fuzzew.co";
 export const SITE_TITLE = "fuzzew";
 export const SITE_DESCRIPTION = "Personal blog";
 
-export const EMAIL = "fuzzeew@gmail..com";
+export const EMAIL = "fuzzeew@gmail.com";
