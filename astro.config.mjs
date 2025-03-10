f9e23ff7ad30d66b23248be6d23f4813d03f5677
Fuzzy <fuzzew@gmail.com>
2025-03-10 06:23:10 +0530

    Configure allowed hosts

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,6 +10,7 @@ import { SITE_URL } from "./src/consts";
 // https://astro.build/config
 export default defineConfig({
   server: {
+    allowedHosts: ['fuzzew.co'],
     host: "0.0.0.0",
     port: parseInt(process.env.PORT || "") || 4321,
   },
